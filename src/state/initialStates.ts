c28f275d52978575882dd17761b8825336d3ec13
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2024-08-11 00:01:43 -0500

    a ver

--- src/state/initialStates.ts
+++ src/state/initialStates.ts
@@ -2,7 +2,7 @@ import { User } from '../data/Entities/User';
 import { UserState } from '../data/Entities/UserLogin';
 import { baseStorage } from '../services/baseStorage';
 const { GetData }= baseStorage();
-export const initialUser: UserState = GetData<UserState>('user') || {
+export const initialUser: UserState = GetData<UserState>('user') ?? {
     Name: "",
     Role: "",
     IsLogged: false
