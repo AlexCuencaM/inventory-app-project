ee55b9a3a2db2e771a72141c8009e24ace603e31
katamile <71morella@gmail.com>
2024-08-10 23:54:15 -0500

    Fixes

--- src/pages/Users/components/UserCreate.tsx
+++ src/pages/Users/components/UserCreate.tsx
@@ -4,14 +4,9 @@ import { FormDialog } from "../../../ui/Dialog/FormDialog";
 
 export const UserCreate = () => {
   const {openCreateDialog, handleOpenCreateDialog, handleCloseCreateDialog} = useDialog();
-
-  const { handleClickOpen } = useDialog();
   const handleClick = () => {
     handleOpenCreateDialog();
   }
-    handleClickOpen();
-  };
-
   return (
     <>
         <Paper sx={{marginBottom: 6}}>
@@ -35,85 +30,6 @@ export const UserCreate = () => {
                     // }}
                 />
         </FormDialog>
-    <Box sx={{ marginBottom: 3, display: 'flex', justifyContent: 'flex-end' }}>
-      <Button
-        onClick={handleClick}
-        variant="contained" 
-        size="small"         
-        sx={{
-          fontSize: '0.75rem', 
-          padding: '4px 8px',  
-          width:150,
-          height:40
-        }}
-      >
-        Crear usuario
-      </Button>
-    </Box>
-
-      <FormDialog
-        title={"Crear usuario"}
-        handleSubmit={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      >
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          {/* <TextField
-            variant="filled"
-            fullWidth
-            name="Username"
-            label="Usuario"
-            placeholder="ej: usuario123"
-            required
-            // value={form.Username}
-            // onChange={e => {
-            //     setForm(e.target.value, "Username")
-            // }}
-          /> */}
-          <TextField
-            variant="filled"
-            fullWidth
-            name="Nombres"
-            label="Nombres"
-            placeholder="ej: Juan"
-            required
-            
-          />
-          <TextField
-            variant="filled"
-            fullWidth
-            name="Apellidos"
-            label="Apellidos"
-            placeholder="ej: Pérez"
-            required
-          />
-          <TextField
-            variant="filled"
-            fullWidth
-            name="Correo"
-            label="Correo"
-            placeholder="ej: usuario@ejemplo.com"
-            required
-          />
-          <TextField
-            variant="filled"
-            fullWidth
-            name="Username"
-            label="Usuario"
-            placeholder="ej: usuario123"
-            required
-          />
-          <TextField
-            variant="filled"
-            fullWidth
-            name="Contraseña"
-            label="Contraseña"
-            type="password"
-            placeholder="********"
-            required
-          />
-        </Box>
-      </FormDialog>
     </>
   );
 };
